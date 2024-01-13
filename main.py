e56effa150492d66a907f6cd4eedbd3a6a01ea3e
Valerie <valeria_serebrennikova@mail.ru>
2024-01-13 17:07:34 +0300

    Finished with genre adding / removing

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ user_sheet = book["User-Genre"]
 bot = telebot.TeleBot(my_token.telebot_token)
 bot.remove_webhook()
 
-# user_genres = []
-vacant_genres = []
-
-current_user = None
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -28,20 +23,19 @@ def start(message):
     markup.add(support, genres, predict)
 
     bot.send_message(message.chat.id, f'Добрый день, {message.from_user.first_name}!\n'
-                                      f'Этот бот создан для того, чтобы помочь Вам осознать собственное настроение и '
+                                      f'\nЭтот бот создан для того, чтобы помочь Вам осознать собственное настроение и '
                                       f'на его основе составить музыкальный плейлист, который Вам точно понравится.\n'
-                                      f'Вы можете начать работу с ним прямо сейчас или сначала настроить свои любимые '
-                                      f'жанры, чтобы наша подборка понравилась Вам еще сильнее!', reply_markup=
-                                      markup)
+                                      f'\nВы можете начать работу с ним прямо сейчас или сначала настроить свои любимые '
+                                      f'жанры, чтобы наша подборка понравилась Вам еще сильнее!', reply_markup=markup)
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAELKLZloTEXxO9gsgWaPcTka2f-xskchQAC-BYAAstG0UpJuy3W8rP2XDQE')
 
 
 def add_user(xlsx_user_id, user_id):
     for i in range(15):
         if i == 0:
-            user_sheet.cell(row=xlsx_user_id, column=i + 1).value = user_id
+            user_sheet.cell(row=xlsx_user_id, column=i+1).value = user_id
         else:
-            user_sheet.cell(row=xlsx_user_id, column=i + 1).value = 0
+            user_sheet.cell(row=xlsx_user_id, column=i+1).value = 0
     book.save('Book.xlsx')
 
 
@@ -91,7 +85,7 @@ def genres_cmd(message):
 
 def predict_cmd(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    menu = types.KeyboardButton("Вернуться в меню")
+    menu = types.KeyboardButton("Вернуться на главную страницу")
     markup.add(menu)
 
     bot.send_message(message.chat.id, f'Если Вы пришлете свою фотографию в этот диалог, наша нейронная сеть '
@@ -117,13 +111,15 @@ def return_cmd(message):
 
 def support_cmd(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    menu = types.KeyboardButton("Вернуться в меню")
+    menu = types.KeyboardButton("Вернуться на главную страницу")
     markup.add(menu)
 
     bot.send_message(message.chat.id, f'Бот был создан совместно студентами третьего курса Университета ИТМО '
                                       f'Пряничниковым К. С. и Серебренниковой В. В.!🕊 \n'
-                                      f'По любому вопросу можете обратиться сюда: @needlessbeating',
+                                      f'По любому вопросу можете обратиться сюда: @needlessbeating\n'
+                                      f'Страница бота в гитхабе: https://github.com/larevies/Music-Bot',
                      reply_markup=markup)
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELK5VlopZR9Q_e89V8bPfoA0jZV-tnbQACShUAApYz0UrkbnOpOGIBNzQE")
 
 
 def add_cmd(message):
@@ -136,18 +132,20 @@ def add_cmd(message):
             all_genres += ", "
 
     bot.send_message(message.chat.id, f'Следующие жанры учтены в боте: {all_genres}.\n'
-                                      f'Чтобы добавить жанр, отправьте его название в следующем сообщении:')
+                                      f'\nЧтобы добавить жанр, отправьте его название в следующем сообщении:')
 
     bot.register_next_step_handler(message, pick_genre)
 
 
 def find_genre(message):
+
     i = 1
     for column in genre_sheet.iter_cols(1):
         for cell in column:
             i += 1
             if cell.value == message.text:
                 return i
+    return 0
 
 
 def find_first_vacant():
@@ -159,17 +157,34 @@ def find_first_vacant():
 
 
 def pick_genre(message):
-    try:
-        user_id = message.from_user.id
-        genre = find_genre(message)
-        user = find_user(user_id)
-        print(f'user = {user}, genre = {genre}')
-        user_sheet.cell(row=user, column=genre).value = 1
-        book.save('Book.xlsx')
 
-    except Exception as e:
-        print(e)
-    bot.send_message(message.chat.id, "Успех!")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    genres = types.KeyboardButton("Жанры")
+    menu = types.KeyboardButton("Вернуться на главную страницу")
+
+    markup.add(genres, menu)
+
+    user_id = message.from_user.id
+    genre = find_genre(message)
+    user = find_user(user_id)
+
+    if genre == 0:
+        bot.send_message(message.chat.id, "Неверно введен жанр!")
+        bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELK41lopW_7BoLbchvDWDqe9AyCyAungACCxMAAmUaQEv-syxD_8aWvzQE")
+        bot.send_message(message.chat.id, "Чтобы начать заново, ответьте \"Жанры\"", reply_markup=markup)
+
+    print(f'user = {user}, genre = {genre}')
+    user_sheet.cell(row=user, column=genre).value = 1
+    book.save('Book.xlsx')
+
+    user_genres = get_user_genres(user)
+
+    bot.send_message(message.chat.id, f'Успех! \n\n'
+                                      f'Жанр \"{message.text}\"'
+                                      f'добавлен в любимые жанры. \n\nВаш обновленный список любимых жанров:\n'
+                                      f'{user_genres}')
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELK5FlopYFSAABVW40d_c2odXFWWLRJX8AAmAWAALsAAHoSgUP7GaqRHiVNAQ")
+    bot.send_message(message.chat.id, "Чтобы начать заново, ответьте \"Жанры\"", reply_markup=markup)
 
 
 @bot.message_handler(content_types='text')
@@ -189,6 +204,9 @@ def message_reply(message):
     elif message.text == "Добавить":
         add_cmd(message)
 
+    elif message.text == "Убрать":
+        remove_cmd(message)
+
 
 def get_user_genres(user_id):
     user_genres = []
@@ -200,14 +218,16 @@ def get_user_genres(user_id):
         if i == 0 or user_genres[i] == 0:
             continue
         else:
-            if i == 14:
-                user_genres_names += user_sheet.cell(2, i + 1).value
-                user_genres_names += ", "
-            else:
-                user_genres_names += user_sheet.cell(2, i+1).value
+            user_genres_names += user_sheet.cell(2, i+1).value
+            user_genres_names += ", "
+    if len(user_genres_names) > 2:
+        user_genres_names = user_genres_names[:-2]
 
+    print("user genres")
     print(user_genres)
+    print("user genres names")
     print(user_genres_names)
+
     return user_genres_names
 
 
@@ -216,147 +236,44 @@ def get_all_genres(sheet):
     return col
 
 
-'''
+def remove_cmd(message):
 
-def pick_time(message):
+    bot.send_message(message.chat.id, f'Чтобы убрать жанр из избранного, отправьте его название в следующем сообщении:')
+    bot.register_next_step_handler(message, remove_genre)
+
+
+def remove_genre(message):
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    back = types.KeyboardButton("На главную страницу")
-    markup.add(back)
-    book = xlrd.open_workbook('Даты.xls', formatting_info=True)
-    if message.text == "На главную страницу":
-        menu(message)
-    elif message.text == "Понедельник":
-        sheet = book.sheet_by_index(0)
-    elif message.text == "Вторник":
-        sheet = book.sheet_by_index(1)
-    elif message.text == "Среда":
-        sheet = book.sheet_by_index(2)
-    elif message.text == "Четверг":
-        sheet = book.sheet_by_index(3)
-    elif message.text == "Пятница":
-        sheet = book.sheet_by_index(4)
-    else:
-        bot.send_message(message.chat.id, "Что-то пошло не так. День недели, который Вы ввели, недоступен.",
-                                          reply_markup=markup)
-        date(message)
-    weekday.clear()
-    weekday.insert(0, message.text)
-    row = sheet.row_values(0)
-    all_times.append(row)
-    bot.send_message(message.chat.id, "Данное время доступно. Введите выбранное время в формате HH:MM, чтобы "
-                                      "продолжить.")
-    bot.send_message(message.chat.id, row, reply_markup=markup)
-    bot.register_next_step_handler(message, time)
+    genres = types.KeyboardButton("Жанры")
+    menu = types.KeyboardButton("Вернуться на главную страницу")
+
+    markup.add(genres, menu)
+
+    user_id = message.from_user.id
+    genre = find_genre(message)
+    user = find_user(user_id)
+
+    if genre == 0:
+        bot.send_message(message.chat.id, "Неверно введен жанр!")
+        bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELK41lopW_7BoLbchvDWDqe9AyCyAungACCxMAAmUaQEv-syxD_8aWvzQE")
+        bot.send_message(message.chat.id, "Чтобы начать заново, ответьте \"Жанры\"", reply_markup=markup)
+
+    print(f'user = {user}, genre = {genre}')
+    user_sheet.cell(row=user, column=genre).value = 0
+    book.save('Book.xlsx')
+
+    user_genres = get_user_genres(user)
+
+    bot.send_message(message.chat.id, f'Успех! \n\n'
+                                      f'Жанр \"{message.text}\" убран из списка избранных жанров.\n'
+                                      f'\nВаш обновленный список любимых жанров:\n{user_genres}')
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAELK5NlopY3RYNsXY1SqmTy5AqLjcVtTgACpRcAAslE0Uvb7l5uWawiazQE")
+    bot.send_message(message.chat.id, "Чтобы начать заново, ответьте \"Жанры\"", reply_markup=markup)
 
 
-def time(message):
-    c = bool()
-    chosen_time.clear()
-    chosen_time.append(message.text)
-    result = []
-    for element in all_times:
-        if type(element) is list:
-            result += element
-        else:
-            result.append(element)
-    all_times_string = ''.join(result)
-    chosen_time_string = ''.join(chosen_time)
-    if all_times_string.find(chosen_time_string) != -1:
-        c = True
-    all_times.clear()
-    if c:
-        bot.send_message(message.chat.id, "Выбранное время: " + message.text)
-        mail_check(message)
-    else:
-        bot.send_message(message.chat.id, "Данное время недоступно, либо то, что вы ввели, не является временем.")
-        date(message)
 
 
-def num(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    back = types.KeyboardButton("На главную страницу")
-    markup.add(back)
-    bot.send_message(message.chat.id, "Теперь введите номер заказа, совершенный с почты " + ''.join(mail) + ".",
-                     reply_markup=markup)
-    if message.text == 'На главную страницу':
-        menu(message)
-    else:
-        bot.register_next_step_handler(message, regex_num)
-
-
-def regex_num(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    back = types.KeyboardButton("На главную страницу")
-    markup.add(back)
-    if message.text == 'На главную страницу':
-        bot.register_next_step_handler(message, menu)
-    elif re.compile(r'\d{6}').match(message.text):
-        number.clear()
-        number.insert(0, message.text)
-        bot.send_message(message.chat.id, "Спасибо!")
-        validation(message)
-    else:
-        bot.send_message(message.chat.id, "Упс :( Указанный номер заказа не валиден.", reply_markup=markup)
-        num(message)
-
-
-def validation(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    yes = types.KeyboardButton("Верно")
-    no = types.KeyboardButton("Неверно")
-    markup.add(yes, no)
-    if message.text == 'На главную страницу':
-        menu(message)
-    bot.send_message(message.chat.id, "Теперь, пожалуйста, проверьте введенные данные:\nПочта: " + ''.join(mail) +
-                     "\nНомер заказа :" + ''.join(number), reply_markup=markup)
-    bot.register_next_step_handler(message, yes_or_no)
-
-
-def yes_or_no(message):
-    if message.text == "Верно":
-        signup = "\n" + ''.join(weekday) + "\n" + ''.join(chosen_time) + "\n" + ''.join(mail) + "\n" + ''.join(number) \
-                 + "\n"
-        f = open("signup.txt", "a")
-        f.write(signup)
-        f.close()
-        bot.send_message(message.chat.id, "Спасибо за бронирование времени! Как только оператор одобрит Ваш заказ, "
-                                          "курьер приедет к Вам!")
-        menu(message)
-    elif message.text == "Неверно":
-        mail.clear()
-        number.clear()
-        all_times.clear()
-        chosen_time.clear()
-        weekday.clear()
-        bot.send_message(message.chat.id, "Придётся начать сначала(")
-        date(message)
-
-
-def menu(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    support = types.KeyboardButton("Поддержка")
-    pick_a_date = types.KeyboardButton("Выбрать дату")
-    info = types.KeyboardButton("Информация")
-    markup.add(support, pick_a_date, info)
-    photo = open("whatnext.jpg", 'rb')
-    bot.send_photo(message.chat.id, photo)
-    bot.send_message(message.chat.id, f'Добрый день, {message.from_user.first_name}! Что бы Вы хотели сделать?',
-                     reply_markup=markup)
-    bot.register_next_step_handler(message, menu_jumper)
-
-
-def menu_jumper(message):
-    if message.text == "Выбрать дату":
-        date(message)
-    elif message.text == "Информация":
-        information(message)
-    elif message.text == "Поддержка":
-        helper(message)
-    else:
-        bot.send_message(message.chat.id, "Возникла ошибка. Такой команды нет в меню, попробуйте ещё раз.")
-        menu(message)
-
-'''
 if __name__ == '__main__':
     try:
         bot.polling(none_stop=True)
